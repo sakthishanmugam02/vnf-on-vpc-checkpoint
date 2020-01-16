24954877e3e4290be99830e03267fd3a242db662
Sakthi <sakshan1@in.ibm.com>
2020-01-16 21:08:50 +0530

    hard-coding removed

--- variable.tf
+++ variable.tf
@@ -31,9 +31,9 @@ variable "region" {
   description = "The VPC Region that you want your VPC, networks and the F5 virtual server to be provisioned in. To list available regions, run `ibmcloud is regions`."
 }
 
-variable "subnet_id"{
-  default = ""
-  description =" The id of the subnet where F5-BIGIP VSI to be provisioned."
+variable "subnet_id" {
+  default     = ""
+  description = " The id of the subnet where F5-BIGIP VSI to be provisioned."
 }
 
 variable "ssh_key_name" {
@@ -44,4 +44,25 @@ variable "ssh_key_name" {
 variable "zone" {
   default     = "us-south-1"
   description = "The VPC Zone that you want your VPC networks and virtual servers to be provisioned in. To list available zones, run `ibmcloud is zones`."
+}
+
+variable "ibmcloud_svc_api_key" {
+  default     = ""
+  type        = "string"
+  description = "The APIKey of the IBM Cloud service account that is hosting the F5-BIGIP qcow2 image file. This should be a the API Key of a Service ID in the account"
+}
+
+variable "vnf_cos_image_url" {
+  default     = ""
+  description = "The COS image object SQL URL for F5-BIGIP qcow2 image."
+}
+
+variable "vnf_cos_instance_id" {
+  default     = ""
+  description = "The COS instance-id hosting the F5-BIGIP qcow2 image."
+}
+
+variable "vnf_vpc_image_name" {
+  default     = "checkpoint-sakthi-test1"
+  description = "The name of the F5-BIGIP custom image to be provisioned in your IBM Cloud account."
 }
